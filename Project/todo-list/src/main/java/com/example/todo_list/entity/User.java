23d454ab485ff52679d8d4d882e522a351a3d2a5
jung choi <chlwjd0803@naver.com>
2025-02-20 16:14:11 +0900

    [Test] 기본 User 엔티티, DTO 정의 / 리포지토리, 서비스, api컨트롤러 생성

---
+++ Project/todo-list/src/main/java/com/example/todo_list/entity/User.java
@@ -0,0 +1,45 @@
+package com.example.todo_list.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // unique를 통해 유저이름이 중복되지 않도록 함
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+    // 추후 개발 예정, 일단 모두 null값을 가질 것
+//    @Column
+//    private LocalDateTime createdAt;
+//    @Column
+//    private LocalDateTime updatedAt;
+//    @Column
+//    private LocalDateTime lastLoginAt;
+//    @Column
+//    private String account_status;
+//    @Column
+//    private String phone_number;
+
+}

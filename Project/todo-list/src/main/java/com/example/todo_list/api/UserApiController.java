79ac5beb7e519d15a24f1445f0ff32523886a0d2
CJ <chlwjd0803@naver.com>
2025-02-23 21:13:30 +0900

    [Minor] 로그인, 회원가입 배경영상 추가 / 메인페이지 UI변경 롤백(추후 다시 작업)

--- Project/todo-list/src/main/java/com/example/todo_list/api/UserApiController.java
+++ Project/todo-list/src/main/java/com/example/todo_list/api/UserApiController.java
@@ -1,4 +1,23 @@
 package com.example.todo_list.api;
 
+import com.example.todo_list.dto.UserDto;
+import com.example.todo_list.entity.User;
+import com.example.todo_list.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/todos")
+
 public class UserApiController {
+    @Autowired
+    private UserService userService;
+
+//    @PostMapping("/signup")
+//    public void signup(@RequestBody UserDto dto) {
+//        User added = userService.signup(dto);
+//    }
 }
--- Project/todo-list/src/main/java/com/example/todo_list/service/UserService.java
+++ Project/todo-list/src/main/java/com/example/todo_list/service/UserService.java
@@ -1,4 +1,12 @@
 package com.example.todo_list.service;
 
-public class UserService {
+import com.example.todo_list.dto.UserDto;
+import com.example.todo_list.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService{
+//    public User signup(UserDto dto) {
+//
+//    }
 }
